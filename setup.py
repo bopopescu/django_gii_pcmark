85d30b297d2bb1aecde437a5c93445b85c08b362
ilnurgi <ilnurgi@mail.ru>
2019-06-28 19:42:05 +0300

    0.0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as fh:
 
 setuptools.setup(
     name='django_gii_pcmark',
-    version='0.0.8',
+    version='0.0.9',
     author='Ильнур Гайфутдинов',
     author_email='ilnurgi87@gmail.com',
     description='Бенчмарки pc комплектующих',
