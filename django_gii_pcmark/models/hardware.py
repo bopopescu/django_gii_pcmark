bd9c71ad2558eb9acff319a6191cc95fee48b0e9
ilnurgi <ilnurgi@mail.ru>
2019-06-30 10:29:34 +0300

    vc str extend

--- django_gii_pcmark/models/hardware.py
+++ django_gii_pcmark/models/hardware.py
@@ -357,19 +357,23 @@ class VideoCard(models.Model):
         строкове представление объекта
         """
         return (
-            '{gpu} ({ram_version} | {ram_bit} bit | {ram_size})'
-            '{freq1}{freq2}'.format(
+            '{gpu}{gpu_freq} ({ram_version} | {ram_bit} bit | {ram_size} | {freq1}{freq2}'.format(
                 gpu=self.gpu,
                 ram_version=self.ram_version,
                 ram_bit=self.ram_bit,
                 ram_size=self.ram_size,
+                gpu_freq=(
+                    ' {0}/{1}'.format(self.gpu_frequency or '-', self.gpu_frequency_max or '-')
+                    if self.gpu_frequency or self.gpu_frequency_max
+                    else ''
+                ),
                 freq1=(
-                    '{0}/{1}'.format(self.ram_frequency_min or '-', self.ram_speed_min or '-')
+                    ' {0}/{1}'.format(self.ram_frequency_min or '-', self.ram_speed_min or '-')
                     if self.ram_frequency_min or self.ram_speed_min
                     else ''
                 ),
                 freq2=(
-                    '{0}/{1}'.format(self.ram_frequency_max or '-', self.ram_speed_max or '-')
+                    ' {0}/{1}'.format(self.ram_frequency_max or '-', self.ram_speed_max or '-')
                     if self.ram_frequency_max or self.ram_speed_max
                     else ''
                 ),
